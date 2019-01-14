939312724e7a2049eca936232316720c135db443
George <gcwebster@hotmail.co.uk>
2019-01-14 16:55:08 +0000

    finished weather

--- bunk-challenge/src/app/home/home.component.ts
+++ bunk-challenge/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface IWeather{
   weather: string;
@@ -14,12 +15,38 @@ interface IWeather{
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private weather: Observable<IWeather[]>;
+  private bristol: Observable<IWeather>;
+  private london: Observable<IWeather>;
+  private rome: Observable<IWeather>;
   private message = "hello";
+  private error = false;
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-      this.weather = this.http.get<IWeather[]>('https://api.openweathermap.org/data/2.5/weather?id=4219762&appid=f4bbeb8ec1a8fb95591f2cf112aaa575');
-  }
+  public ngOnInit() {
+
+    this.bristol = this.http.get<IWeather>('https://api.openweathermap.org/data/2.5/weather?id=4749005&appid=f4bbeb8ec1a8fb95591f2cf112aaa575')
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        this.error=true;
+        return new Observable<IWeather>();
+      })
+    );
+
+    this.london = this.http.get<IWeather>('https://api.openweathermap.org/data/2.5/weather?id=2643744&appid=f4bbeb8ec1a8fb95591f2cf112aaa575')
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        this.error=true;
+        return new Observable<IWeather>();
+      })
+    );
+
+    this.rome = this.http.get<IWeather>('https://api.openweathermap.org/data/2.5/weather?id=4219762&appid=f4bbeb8ec1a8fb95591f2cf112aaa575')
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        this.error=true;
+        return new Observable<IWeather>();
+      })
+    );
+  };
 
 }
